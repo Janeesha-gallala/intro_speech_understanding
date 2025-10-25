0bc1cfd132131889068c7a6e080657b438faf821
unknown <st002441@m2.kcg.edu>
2025-10-26 04:13:15 +0900

    hw2 complete

--- lec02/homework2.py
+++ lec02/homework2.py
@@ -14,3 +14,12 @@ def arithmetic(x, y):
     """
     return 0
 
+def arithmetic(x, y):
+    if isinstance(x, str) and isinstance(y, str):
+        return x + y
+    elif isinstance(x, str) and isinstance(y, float):
+        return x * int(y)
+    elif isinstance(x, float) and isinstance(y, str):
+        return str(x) + y
+    elif isinstance(x, float) and isinstance(y, float):
+        return x * y
