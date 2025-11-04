8894ec1351ed389d8459151b0a034aa92ea2f6e7
unknown <st002441@m2.kcg.edu>
2025-11-04 22:37:41 +0900

    Homework 4 completed successfully

--- lec04/homework4.py
+++ lec04/homework4.py
@@ -1,6 +1,5 @@
-
 def next_birthday(date, birthdays):
-    '''
+    """
     Find the next birthday after the given date.
 
     @param:
@@ -11,8 +10,20 @@ def next_birthday(date, birthdays):
     @return:
     birthday - the next day, after given date, on which somebody has a birthday
     list_of_names - list of all people with birthdays on that date
-    '''
-    birthday = (1,1)
-    list_of_names = []
-    return birthday, list_of_names
+    """
+    if not birthdays:
+        return (1, 1), []
+
+    # Sort all birthday dates
+    all_dates = sorted(birthdays.keys())
+
+    # Try to find a birthday after the given date
+    for d in all_dates:
+        if d[0] > date[0] or (d[0] == date[0] and d[1] > date[1]):
+            return d, birthdays[d]
+
+    # If not found, wrap around to the first birthday in the next year
+    first = all_dates[0]
+    return first, birthdays[first]
+
     
