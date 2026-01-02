7023032e393318ae67c9f6c7ced4ccb1e793f61a
unknown <st002441@m2.kcg.edu>
2026-01-02 22:47:30 +0900

    lec07 complete

--- lec07/homework7.py
+++ lec07/homework7.py
@@ -2,46 +2,54 @@ import numpy as np
 
 def major_chord(f, Fs):
     '''
-    Generate a one-half-second major chord, based at frequency f, with sampling frequency Fs.
-
-    @param:
-    f (scalar): frequency of the root tone, in Hertz
-    Fs (scalar): sampling frequency, in samples/second
-
-    @return:
-    x (array): a one-half-second waveform containing the chord
-    
-    A major chord is three notes, played at the same time:
-    (1) The root tone (f)
-    (2) A major third, i.e., four semitones above f
-    (3) A major fifth, i.e., seven semitones above f
+    Generate a one-half-second major chord, based at frequency f,
+    with sampling frequency Fs.
     '''
-    raise RuntimeError("You need to write this part")
+    N = int(0.5 * Fs)
+    n = np.arange(N)
+
+    # Major chord frequencies
+    f1 = f
+    f2 = f * (2 ** (4/12))
+    f3 = f * (2 ** (7/12))
+
+    x = (
+        np.cos(2 * np.pi * f1 * n / Fs) +
+        np.cos(2 * np.pi * f2 * n / Fs) +
+        np.cos(2 * np.pi * f3 * n / Fs)
+    )
+    return x
+
 
 def dft_matrix(N):
     '''
-    Create a DFT transform matrix, W, of size N.
-    
-    @param:
-    N (scalar): number of columns in the transform matrix
-    
-    @result:
-    W (NxN array): a matrix of dtype='complex' whose (k,n)^th element is:
-           W[k,n] = cos(2*np.pi*k*n/N) - j*sin(2*np.pi*k*n/N)
+    Create a DFT transform matrix of size N.
     '''
-    raise RuntimeError("You need to write this part")
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    W = np.cos(2 * np.pi * k * n / N) - 1j * np.sin(2 * np.pi * k * n / N)
+    return W
+
 
 def spectral_analysis(x, Fs):
     '''
     Find the three loudest frequencies in x.
-
-    @param:
-    x (array): the waveform
-    Fs (scalar): sampling frequency (samples/second)
-
-    @return:
-    f1, f2, f3: The three loudest frequencies (in Hertz)
-      These should be sorted so f1 < f2 < f3.
     '''
-    raise RuntimeError("You need to write this part")
+    N = len(x)
 
+    W = dft_matrix(N)
+    X = np.dot(W, x)
+    mag = np.abs(X)
+
+    # Keep only positive frequencies (avoid mirrored peaks)
+    mag = mag[:N // 2]
+
+    # Remove DC component
+    mag[0] = 0
+
+    # Find three strongest peaks
+    idx = np.argsort(mag)[-3:]
+    freqs = idx * Fs / N
+
+    freqs = np.sort(freqs)
+    return freqs[0], freqs[1], freqs[2]
