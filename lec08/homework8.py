f943ca6cb9857563a765fa82484f746fb470964a
unknown <st002441@m2.kcg.edu>
2026-01-02 23:30:47 +0900

    lec08

--- lec08/homework8.py
+++ lec08/homework8.py
@@ -1,45 +1,38 @@
+
 import numpy as np
 
 def waveform_to_frames(waveform, frame_length, step):
-    '''
-    Chop a waveform into overlapping frames.
-    
-    @params:
-    waveform (np.ndarray(N)) - the waveform
-    frame_length (scalar) - length of the frame, in samples
-    step (scalar) - step size, in samples
-    
-    @returns:
-    frames (np.ndarray((num_frames, frame_length))) - waveform chopped into frames
-       frames[m/step,n] = waveform[m+n] only for m = integer multiple of step
-    '''
-    raise RuntimeError("You need to change this part")
+    N = len(waveform)
+    num_frames = 1 + (N - frame_length) // step
+
+    frames = np.zeros((num_frames, frame_length))
+
+    for i in range(num_frames):
+        start = i * step
+        frames[i, :] = waveform[start:start + frame_length]
+
+    return frames
+
 
 def frames_to_mstft(frames):
-    '''
-    Take the magnitude FFT of every row of the frames matrix.
-    
-    @params:
-    frames (np.ndarray((num_frames, frame_length))) - the speech samples
-    
-    @returns:
-    mstft (np.ndarray((num_frames, frame_length))) - the magnitude short-time Fourier transform
-    '''
-    raise RuntimeError("You need to change this part")
+    num_frames, frame_length = frames.shape
+    mstft = np.zeros((num_frames, frame_length))
+
+    for i in range(num_frames):
+        X = np.fft.fft(frames[i])
+        mstft[i] = np.abs(X)
+
+    return mstft
+
 
 def mstft_to_spectrogram(mstft):
-    '''
-    Convert max(0.001*amax(mstft), mstft) to decibels.
-    
-    @params:
-    stft (np.ndarray((num_frames, frame_length))) - magnitude short-time Fourier transform
-    
-    @returns:
-    spectrogram (np.ndarray((num_frames, frame_length)) - spectrogram 
-    
-    The spectrogram should be expressed in decibels (20*log10(mstft)).
-    np.amin(spectrogram) should be no smaller than np.amax(spectrogram)-60
-    '''
-    raise RuntimeError("You need to change this part")
+    max_val = np.amax(mstft)
+    threshold = max(0.001 * max_val, 1e-12)
 
+    mstft = np.maximum(mstft, threshold)
+    spectrogram = 20 * np.log10(mstft)
 
+    max_db = np.amax(spectrogram)
+    spectrogram = np.maximum(spectrogram, max_db - 60)
+
+    return spectrogram
